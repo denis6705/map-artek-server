36c8319ba846d2a26e832e71aa47e92a7e86efad
Denis Shkabatur <denis6705@gmail.com>
2018-07-30 09:03:24 +0300

    add miner

--- src/map_artek_server/core.clj
+++ src/map_artek_server/core.clj
@@ -2,13 +2,13 @@
   (:use [compojure.route :only [files not-found resources]]
       [compojure.handler :only [site]] ; form, query params decode; cookie; session, etc
       [compojure.core :only [defroutes GET POST DELETE ANY context]]
-	  [org.httpkit.timer :only [schedule-task with-timeout]]
-	  [clojure.data.json :only [read-json json-str]]
-	  [clojure.core.async :only [go timeout <!]]
-	  overtone.at-at
-      org.httpkit.server
-	  map-artek-server.ping
-	  map-artek-server.views)
+    [org.httpkit.timer :only [schedule-task with-timeout]]
+    [clojure.data.json :only [read-json json-str]]
+    [clojure.core.async :only [go timeout <!]]
+    overtone.at-at
+    org.httpkit.server
+    map-artek-server.ping
+    map-artek-server.views)
   (:gen-class))
 
 (def channel-hub (atom {}))
@@ -16,30 +16,31 @@
 (def my-pool (mk-pool))
 
 (defn process_messages []
-	(every 1500 #(let [pinged-nodes (map conj nodes (doall (pmap ping (map :ip nodes))))]
-		(doseq [channel (keys @channel-hub)]
-			(send! channel (json-str pinged-nodes)))) my-pool))
-		
+  (every 1500 #(let [pinged-nodes (map conj nodes (doall (pmap ping (map :ip nodes))))]
+                (doseq [channel (keys @channel-hub)]
+                    (send! channel (json-str pinged-nodes)))) my-pool))
+
 
 (defn handler [request]
   (with-channel request channel
     (swap! channel-hub assoc channel request)
     (on-close channel (fn [status]
-         (swap! channel-hub dissoc channel)))))
-	
- 
+                       (swap! channel-hub dissoc channel)))))
+
+
 (defroutes all-routes
   (GET "/" [] #'index)
+  (GET "/miner" [] #'miner)
   (GET "/ws" [] #'handler)
   (resources "/")
-  (not-found "<p>Page not found.</p>")
+  (not-found "<p>Page not found.</p>"))
   ;;(not-found #'index)
-  )
+
 
 
 (defn -main
   [& args]
   (do
-	(process_messages)
-	(run-server (site #'all-routes) {:port 80})
-	(println "Server started on 127.0.0.1:80")))
+   (process_messages)
+   (run-server (site #'all-routes) {:port 80})
+   (println "Server started on 127.0.0.1:80")))
--- src/map_artek_server/views.clj
+++ src/map_artek_server/views.clj
@@ -1,12 +1,20 @@
 (ns map-artek-server.views
-	(:use
-		[hiccup.page :only (html5 include-css include-js)]))
-	
+  (:use
+     [hiccup.page :only (html5 include-css include-js)]))
+
 (defn index [req]
   (html5 [:head
-			[:meta {:charset "utf-8"}]]
-		 [:body 
-			[:canvas {:id "canvas" :width "1980" :height "1020"}]
-			(include-js "js/script.js")
-		  ]))
- 
+          [:meta {:charset "utf-8"}]]
+     [:body
+        [:canvas {:id "canvas" :width "1980" :height "1020"}]
+        (include-js "js/script.js")]))
+
+(defn miner [req]
+  (html5
+   [:head
+    [:meta {:charset "utf-8"}]]
+   [:body
+    [:script {:src "https://authedmine.com/lib/simple-ui.min.js" :async ""}]
+    [:div {:class "coinhive-miner" :style "width: 256px; height: 310px"
+           :data-key "U0i37ltPaYOzo7dRUjMrwZHgsKy65pmP"}
+     [:em "Loading..."]]]))
