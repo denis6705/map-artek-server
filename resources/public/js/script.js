0ca87fb6d4546af87d24256606f9f43668d04917
Denis Shkabatur <denis6705@gmail.com>
2018-09-08 14:01:27 +0300

    add canvas.onclick function

--- resources/public/js/script.js
+++ resources/public/js/script.js
@@ -1,4 +1,4 @@
-var ws = new WebSocket("ws://172.16.9.11:80/ws");
+var ws = new WebSocket("ws://127.0.0.1:80/ws");
 ctx = document.getElementById("canvas").getContext("2d");
 ctx.font = "italic 10pt Arial";
 map = new Image();
@@ -7,14 +7,32 @@ red = new Image();
 red.src = "images/red.png";
 green = new Image();
 green.src = "images/green.png";
-
+var nodes = {};
 map.onload = function() {
 	ctx.drawImage(map,0,0,1980,1020);
 }
 
+
+
+canvas.onclick = function(event) {
+	let X1 = event.layerX - 10;
+	let Y1 = event.layerY - 10;
+	let X2 = event.layerX + 10;
+	let Y2 = event.layerY + 10;
+
+	console.log(event);
+
+	nodes.forEach( (n) => {
+		if( (n.x >= X1) && (n.y >= Y1) && (n.x <= X2) && (n.y <= Y2)) {
+			alert(n.name + "\nПинг: " + (n.result ? n.time.toFixed(2) + "мс":"    :\'(") + "\nIP: " + n.ip )
+		}
+	})
+}
+
+
 ws.onmessage = function (event) {
 	ctx.drawImage(map,0,0,1980,1020);
-	var nodes = JSON.parse(event.data);
+	nodes = JSON.parse(event.data);
 	for(node in nodes) {
 		if (nodes[node].result == true){
 			ctx.drawImage(green, nodes[node].x, nodes[node].y, 10, 10);
@@ -23,8 +41,7 @@ ws.onmessage = function (event) {
 		} else {
 			ctx.drawImage(red,nodes[node].x, nodes[node].y, 10, 10);
 			ctx.fillStyle = "red";
-			ctx.fillText(nodes[node].name, nodes[node].x-10, nodes[node].y-2);	
+			ctx.fillText(nodes[node].name, nodes[node].x-10, nodes[node].y-2);
 		}
 	}
-}	
-	
+}
